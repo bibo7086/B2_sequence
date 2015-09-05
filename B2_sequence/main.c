de8eb112a031faf1852bc764d21aa4e9721cda8a
Saeed Aliyu <saeedaliyu@Saeeds-MacBook-Pro.local>
2015-09-05 20:32:55 +0100

    Initial Commit

---
+++ B2_sequence/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  B2_sequence
+//
+//  Created by Saeed Aliyu on 9/5/15.
+//  Copyright (c) 2015 Saeed Aliyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
